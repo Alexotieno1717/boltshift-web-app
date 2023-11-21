c22161b5626d231e851ba457f41f4b30ea418789
Alexotieno1717 <alexotieno900@gmail.com>
2023-11-21 10:11:38 +0300

    [chore]: complete husky setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'next',
-    'next/core-web-vitals',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh', '@typescript-eslint'],
