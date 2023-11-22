f8d921c33c02df0ac553e42a6cff290cd03fe2a8
Alexotieno1717 <alexotieno900@gmail.com>
2023-11-22 18:30:39 +0300

    [bug]: having bug on navigation

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -4,15 +4,15 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className="flex justify-center items-center content-center gap-16 flex-wrap pt-6">
+    <div className="lg:max-w-7xl mx-auto p-4 bg-white px-8">
       <div className="flex flex-row p-2 items-center">
         <Bars3BottomLeftIcon className="h-6 w-6" />
         <div className="flex pl-3 gap-3">
           <Image src="/Logomark.svg" width={44} height={44} alt="logomark" />
           <Image src="/Logotype.svg" width={115} height={23} alt="logotype" />
         </div>
-        <div className="items-center pl-4">
-          <form action="" className="flex">
+        <div className="lg:max-w-4xl items-center pl-4">
+          <form action="" className="flex lg:max-w-4xl">
             <input type="text" placeholder="Search for items" className="border border-[#D0D5DD] rounded-md" />
             <button className="flex p-2 justify-center items-center gap-8 text-white border-t-0 rounded-md border-r-0 border-b-0 border-l-0 border-solid border-1 border-[#870064] bg-[#870064] shadow-xs">
               Search
