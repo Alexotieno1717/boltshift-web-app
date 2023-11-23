2409f990365df92c1d7c30f08d6040f753f1c312
Alexotieno1717 <alexotieno900@gmail.com>
2023-11-23 11:24:59 +0300

    [doc]: Added data for products

---
+++ src/constant/index.ts
@@ -0,0 +1,47 @@
+const products = [
+  {
+    imageSrc: 'deals/Product_Image.png',
+    title: 'Modern White Velvet 3-Seater Sofa',
+    price: '92,372',
+    availability: '67',
+  },
+  {
+    imageSrc: 'deals/Product_Image2.png',
+    title:
+      'Contemporary Accent Table Nightstand Drawer Storage Bedside Cabinet - 16"L x 16"W x 18"H Off-White 2 Piece Set Nightstands',
+    price: '54,436',
+    availability: '28',
+  },
+  {
+    imageSrc: 'deals/Product_Image3.png',
+    title: 'Flower Moon Spray Perfume',
+    price: '63,073',
+    availability: '56',
+  },
+  {
+    imageSrc: 'deals/Product_Image4.png',
+    title: 'TEVISE T845 Casual Style Men Wrist Watch Date Display Full Steel Band Quartz Watch - NO.2',
+    price: '80,020',
+    availability: '85',
+  },
+  {
+    imageSrc: 'deals/Product_Image5.png',
+    title: "Men's Jacquard Pullover Hoodie - Navy / L",
+    price: '70,977',
+    availability: '32',
+  },
+  {
+    imageSrc: 'deals/Product_Image6.png',
+    title: 'Candy Color Pointed Toe Low Cut Stiletto High Heels Prom Shoes',
+    price: '56,536',
+    availability: '56',
+  },
+  {
+    imageSrc: 'deals/Product_Image7.png',
+    title: 'Modern Chic Round Nesting Coffee Table with Storage White Stone Coffee Table Set of 2',
+    price: '95,434',
+    availability: '18',
+  },
+];
+
+export default products;
